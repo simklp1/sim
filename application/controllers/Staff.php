7d5bbfa0a284a8f6004a172b516f8d0051fa8748
majiddul <majidblk22@gmail.com>
2018-07-13 12:16:08 +0800

    perbaikan pada controller staff
    perbaikan pada controller staff

--- application/controllers/Staff.php
+++ application/controllers/Staff.php
@@ -206,5 +206,178 @@ class Staff extends CI_Controller {
         $this->M_staff->update_data($where,$data,'pj');
         redirect('Staff/tampil_pj');
     }
-}
+
+    function tampil_pegawai(){
+        $data['jabatan'] = $this->M_staff->tampil_data('jabatan');
+        $this->load->view('Staff/header',$data);
+        $data['pegawai']=$this->M_staff->tampil_data('pegawai');
+        $this->load->view('Staff/v_pegawai',$data);
+        $this->load->view('Staff/footer');
+    }
+
+    function tambah_data_pegawai(){
+        $data['jabatan'] = $this->M_staff->tampil_data('jabatan');
+        $this->load->view('Staff/header',$data);
+        $data['jabatan'] = $this->M_staff->tampil_data('jabatan');
+        $this->load->view('Staff/input_datapegawai',$data);
+        $this->load->view('Staff/footer');
+    }
+
+     function tambah_aksi_pegawai(){
+        $nama           = $this->input->post('nama');
+        $ktp            = $this->input->post('ktp');
+        $id_jabatan     = $this->input->post('id_jabatan');
+        $pangkat        = $this->input->post('pangkat');
+        $alamat         = $this->input->post('alamat');
+        $telpon         = $this->input->post('telpon');
+        $tempat_lahir   = $this->input->post('tempat_lahir');
+        $tanggal_lahir  = $this->input->post('tanggal_lahir');
+        $agama          = $this->input->post('agama');
+        $pendidikan     = $this->input->post('pendidikan');
+                $data = array(
+                    'nama'            => $nama,
+                    'ktp'             => $ktp,
+                    'id_jabatan'      => $id_jabatan,
+                    'pangkat'         => $pangkat,
+                    'alamat'          => $alamat,
+                    'telpon'          => $telpon,
+                    'tempat_lahir'    => $tempat_lahir,
+                    'tanggal_lahir'   => $tanggal_lahir,
+                    'agama'           => $agama,
+                    'pendidikan'      => $pendidikan
+
+                );
+
+         $where=array(
+            'id' => $id);
+
+        $this->M_staff->input_data($data,'pegawai');
+        redirect ('Staff/tampil_pegawai');
+    }
+
+     public function hapus_data_pegawai($id){
+        $where = array('id' => $id);
+        $this->M_staff->hapus_data($where,'pegawai');
+        redirect('Staff/tampil_pegawai');
+    }
+
+    function edit_data_pegawai($id){
+        $where = array(
+            'id' => $id
+        );
+        $data['jabatan'] = $this->M_staff->tampil_data('jabatan');
+        $this->load->view('Staff/header',$data);
+        $data['pegawai'] = $this->M_staff->edit_data($where,'pegawai')->result();
+        $data['jabatan']    = $this->M_staff->tampil_data('jabatan');
+        $this->load->view('Staff/edit_datapegawai',$data);
+        $this->load->view('Staff/footer');
+    }
+
+    function update_data_pegawai(){
+        $id             = $this->input->post('id');
+        $nama           = $this->input->post('nama');
+        $ktp            = $this->input->post('ktp');
+        $id_jabatan     = $this->input->post('id_jabatan');
+        $pangkat        = $this->input->post('pangkat');
+        $alamat         = $this->input->post('alamat');
+        $telpon         = $this->input->post('telpon');
+        $tempat_lahir   = $this->input->post('tempat_lahir');
+        $tanggal_lahir  = $this->input->post('tanggal_lahir');
+        $agama          = $this->input->post('agama');
+        $pendidikan     = $this->input->post('pendidikan');
+                $data = array(
+                    'id'              => $id,
+                    'nama'            => $nama,
+                    'ktp'             => $ktp,
+                    'id_jabatan'      => $id_jabatan,
+                    'pangkat'         => $pangkat,
+                    'alamat'          => $alamat,
+                    'telpon'          => $telpon,
+                    'tempat_lahir'    => $tempat_lahir,
+                    'tanggal_lahir'   => $tanggal_lahir,
+                    'agama'           => $agama,
+                    'pendidikan'      => $pendidikan
+
+                );
+
+        $where=array(
+            'id' => $id);
+
+        $this->M_staff->update_data($where,$data,'pegawai');
+        redirect('Staff/tampil_pegawai');
+    }
+
+     function tampil_pensiun(){
+        $data['pegawai'] = $this->M_staff->tampil_data('pegawai');
+        $this->load->view('Staff/header',$data);
+        $data['pensiun']=$this->M_staff->tampil_data('pensiun');
+        $this->load->view('Staff/v_pensiun',$data);
+        $this->load->view('Staff/footer');
+    }
+
+     function tambah_data_pensiun(){
+        $data['pegawai'] = $this->M_staff->tampil_data('pegawai');
+        $this->load->view('Staff/header',$data);
+        $data['pegawai'] = $this->M_staff->tampil_data('pegawai');
+        $this->load->view('Staff/input_datapensiun',$data);
+        $this->load->view('Staff/footer');
+    }
+
+     function tambah_aksi_pensiun(){
+        $nama           = $this->input->post('nama');
+        $tanggal_pensiun= $this->input->post('tanggal_pensiun');
+        $deskripsi      = $this->input->post('deskripsi');
+                $data = array(
+                    'nama'            => $nama,
+                    'tanggal_pensiun' => $tanggal_pensiun,
+                    'deskripsi'       => $deskripsi
+
+                );
+
+         $where=array(
+            'id' => $id);
+
+        $this->M_staff->input_data($data,'pensiun');
+        redirect ('Staff/tampil_pensiun');
+    }
+
+     public function hapus_data_pensiun($id){
+        $where = array('id' => $id);
+        $this->M_staff->hapus_data($where,'pensiun');
+        redirect('Staff/tampil_pensiun');
+    }
+
+    function edit_data_pensiun($id){
+        $where = array(
+            'id' => $id
+        );
+        $data['pegawai'] = $this->M_staff->tampil_data('pegawai');
+        $this->load->view('Staff/header',$data);
+        $data['pensiun'] = $this->M_staff->edit_data($where,'pensiun')->result();
+        $data['pegawai'] = $this->M_staff->tampil_data('pegawai');
+        $this->load->view('Staff/edit_datapensiun',$data);
+        $this->load->view('Staff/footer');
+    }
+
+
+    function update_data_pensiun(){
+        $id             = $this->input->post('id');
+        $nama           = $this->input->post('nama');
+        $tanggal_pensiun= $this->input->post('tanggal_pensiun');
+        $deskripsi      = $this->input->post('deskripsi');
+                $data = array(
+                    'id'              => $id,
+                    'nama'            => $nama,
+                    'tanggal_pensiun' => $tanggal_pensiun,
+                    'deskripsi'       => $deskripsi
+
+                );
+
+        $where=array(
+            'id' => $id);
+
+        $this->M_staff->update_data($where,$data,'pensiun');
+        redirect('Staff/tampil_pensiun');
+    }
+} 
 ?>
